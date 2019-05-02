860f78161d4b9e45f2af2af9f7bb59ebac13bc21
Paul Mooney <daishisystems@users.noreply.github.com>
2019-05-02 10:20:26 +0100

    Reduced Quartz threadcount, job frequency

--- src/Strada.Api/ClientConfigSettings.cs
+++ src/Strada.Api/ClientConfigSettings.cs
@@ -7,8 +7,8 @@
         public long ElementCountThreshold { get; set; } = 1000;
         public long RequestByteThreshold { get; set; } = 5242880; // MAX bytes 10485760 / 2
         public int DelayThreshold { get; set; } = 3;
-        public int MaxThreadCount { get; set; } = 45;
-        public int ExecutionTimeInterval { get; set; } = 1;
+        public int MaxThreadCount { get; set; } = 10;
+        public int ExecutionTimeInterval { get; set; } = 30;
         public int MaxQueueLength { get; set; } = 175000;
     }
 }
